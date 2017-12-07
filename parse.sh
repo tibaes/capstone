357b22598ebe7bd13463b1316e9e09f28185c98e
Rafael Henrique Tibaes <r@fael.nl>
2017-12-07 11:33:46 -0200

    using csv format, derp

--- parse.sh
+++ parse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "path, target" > dataset.txt
+echo "path, target" > dataset.csv
 for img in `find ./data/ -iname "*.png"`
 do
     txt=`echo $img | sed -e 's/.png/.txt/'`
     class=`grep -i "class" $txt | cut -d" " -f2`
-    echo "$img, $class" >> dataset.txt
+    echo "$img, $class" >> dataset.csv
 done
